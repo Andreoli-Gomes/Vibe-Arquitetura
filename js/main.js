4deb240f4b763949f8f80c2d0ba86e754fd02d77
Vinicius <249995415+vnkgomes@users.noreply.github.com>
2026-04-13 22:03:58 -0400

    updated animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // Burger Menu Toggle
 const burgerMenu = document.getElementById('burger');
 const navMenu = document.getElementById('nav-menu');
+const header = document.querySelector('header');
 const navLinks = navMenu.querySelectorAll('a');
 
 // Toggle menu on burger click
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
+    header.classList.toggle('menu-open');
 });
 
 // Close menu when a link is clicked
@@ -14,6 +16,7 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         burgerMenu.classList.remove('active');
         navMenu.classList.remove('active');
+        header.classList.remove('menu-open');
     });
 });
 
@@ -25,5 +28,6 @@ document.addEventListener('click', (event) => {
     if (!isClickInsideNav && !isClickInsideBurger && navMenu.classList.contains('active')) {
         burgerMenu.classList.remove('active');
         navMenu.classList.remove('active');
+        header.classList.remove('menu-open');
     }
 });
